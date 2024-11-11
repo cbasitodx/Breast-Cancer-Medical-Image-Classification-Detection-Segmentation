3f5cb43acd5200a1160d426854ab4f38bf4768fb
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-11-11 11:48:25 +0100

    Add documentation to IViewer

--- src/viewer/iviewer.py
+++ src/viewer/iviewer.py
@@ -2,6 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class IViewer(ABC):
+    """
+    ===========
+    **Summary**
+    ===========
+
+        This class is an interface for the SingleViewer class.
+
+    ==================
+    **Public Methods**
+    ==================
+
+        * **plot_img(self):** This method plots the image.
+        * **plot_bounding_box(self):** This method plots the image with the bounding box.
+    """
     @abstractmethod
     def plot_img(self):
         pass
